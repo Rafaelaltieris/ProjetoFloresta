52e20b9713db27e9c4833cf7801d5d2c41ad77bf
Rafael Altieris <raphaelaltieris@gmail.com>
2024-10-24 07:38:59 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/DenunciaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoFloresta.Controllers
+{
+    public class DenunciaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/PerfilController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace ProjetoFloresta.Controllers
+{
+    public class PerfilController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+       
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public PerfilController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                var user = await _userManager.FindByIdAsync(userId);
+                ViewData["UserName"] = User.Identity.Name;
+                ViewData["Email"] = User.FindFirst(ClaimTypes.Email)?.Value;
+                return View();
+            }
+            else
+            {
+                return Redirect("/Identity/Account/Login");
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home"); // Redireciona após o logout
+        }
+    }
+}
